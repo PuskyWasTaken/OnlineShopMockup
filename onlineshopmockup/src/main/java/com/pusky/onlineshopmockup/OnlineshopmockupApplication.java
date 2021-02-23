a6796c7d825ce06d4ebee4dc0ffe1a9580bee68c
Pusky <puscasualexandru10@gmail.com>
2021-02-23 22:35:45 +0200

    Added Spring Initializr generated files.

---
+++ onlineshopmockup/src/main/java/com/pusky/onlineshopmockup/OnlineshopmockupApplication.java
@@ -0,0 +1,13 @@
+package com.pusky.onlineshopmockup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshopmockupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshopmockupApplication.class, args);
+	}
+
+}
---
+++ onlineshopmockup/src/test/java/com/pusky/onlineshopmockup/OnlineshopmockupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pusky.onlineshopmockup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopmockupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
